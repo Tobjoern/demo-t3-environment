ef1690f1b410c395941ed1b8dfc2f68a5e85de1b
Hichem Fantar <hichemfantar2049@gmail.com>
2023-06-16 11:33:47 +0100

    Implement example display and delete

--- src/pages/examples.tsx
+++ src/pages/examples.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { vanillaApi } from '~/utils/api'
+import { api } from "~/utils/api";
 
 export default function Examples() {
-    // vanillaApi.example.getAll()
+  // Task 1: Display the data from the getAll query in a list,
+  //         while it is loading display a loading indicator (i.e. a text saying "Loading...")
+  // Task 2: Add a button to each item in the list that will delete the item, by calling,
+  // the 'deleteSingle' mutation.
+  // Task 3: Implement the 'deleteSingle' mutation, and make sure the list updates when an item is deleted
 
-    // Task 1: Display the data from the getAll query in a list, 
-    //         while it is loading display a loading indicator (i.e. a text saying "Loading...")
-    // Task 2: Add a button to each item in the list that will delete the item, by calling,
-    // the 'deleteSingle' mutation.
-    // Task 3: Implement the 'deleteSingle' mutation, and make sure the list updates when an item is deleted
+  // vanillaApi.example.getAll();
+
+  const ctx = api.useContext();
+
+  const getExamplesQuery = api.example.getAll.useQuery();
+
+  const deleteExampleMutation = api.example.deleteSingle.useMutation({
+    onSuccess: () => {
+      void ctx.example.getAll.invalidate();
+    },
+  });
 
   return (
-    <div>Examples</div>
-  )
+    <div className="p-4">
+      {getExamplesQuery.isSuccess &&
+        getExamplesQuery.data.map((example) => (
+          <div key={example.id} className="mb-4 rounded-xl bg-gray-200 p-4">
+            Example ID: {example.id}
+            <button
+              className="ml-4 rounded-lg bg-red-500 p-2 text-white"
+              onClick={() =>
+                deleteExampleMutation.mutate({
+                  id: example.id,
+                })
+              }
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+    </div>
+  );
 }
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+export const ExampleDeleteInput = z.object({
+  id: z.string(),
+});
+
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
@@ -12,7 +16,13 @@ export const exampleRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
-  deleteSingle: publicProcedure.mutation(({ ctx, input }) => {
-    // TODO: Complete!
-  }),
+  deleteSingle: publicProcedure
+    .input(ExampleDeleteInput)
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.example.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
 });
